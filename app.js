251389881268ec8187f3a9cde94ddd8f5a2313b2
Joel Hdz <joehdzr93@gmail.com>
2025-02-02 13:48:23 -0600

    Actualiza app.js
    Añade función actualizar amigos

--- app.js
+++ app.js
@@ -3,18 +3,40 @@ let listaAmigos = [];
 
 //funcion para anadir un nombre a la lista
 function agregarAmigo() {
-    //obtiene el valor del imput
-    let nombreInput = document.querySelector('#amigo').value;
+    //obtiene el valor del input
+    let nombreInput = document.querySelector('#amigo');
     if (nombreInput === '') {
         alert('Por favor introduce un nombre en el campo');
         return;
     } else {
         listaAmigos.push(nombreInput.value);
         limpiarCaja();
-        cambiaTextoLista();
+        actualizarLista();
     }
 }
 
 function limpiarCaja() {
-    
+    document.querySelector('#amigo').value = '';
+}
+
+function actualizarLista() {
+    let listaAmigosUl = document.querySelector('#listaAmigos');
+    listaAmigosUl.innerHTML = '';
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = listaAmigos[i];
+        listaAmigosUl.appendChild(li);
+        
+    }
+}
+
+function sortearAmigo() {
+
+
+    if (listaAmigos) {
+        
+    } else {
+        
+    }
 }
